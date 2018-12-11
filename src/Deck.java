919e2ef7289481d393f5a69cf7e706165130c1aa
BT_1N3_11 <@>
2018-12-11 10:09:57 -0500

    AndAnotherLast

--- src/Deck.java
+++ src/Deck.java
@@ -6,6 +6,24 @@ public class Deck
     ArrayList<Card> Dealt;
     public Deck(String[] a,String[] b,int[] c)
     {
+
+    }
+    public boolean isEmpty()
+    {
+        return true;
+    }
+    public int size()
+    {
+        return 0;
+    }
+    public void deal()
+    {
+
+    }
+    public void suffle()
+    {
         
     }
+
+
 }
